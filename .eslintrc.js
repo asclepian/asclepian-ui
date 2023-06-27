502e50e5f738d89ee75c5a823eb320fb3304cec0
CHAFIK Wassime <chafik.wassime@gmail.com>
2023-06-27 23:42:32 +0100

    instqlled eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,32 @@
 module.exports = {
-  env: {
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json']
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 }
